83164dc356ea90cad29f6fb47677ba574b0e2e34
joungHyeon <jounghyeon123@daum.net>
2025-04-13 00:55:27 +0900

    style: frontend, bakcend 환경 구성

---
+++ backend/src/main/java/com/example/ecommercewebservice/ECommerceWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebServiceApplication.class, args);
+	}
+
+}
